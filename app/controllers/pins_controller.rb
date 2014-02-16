4d052615690925d1b0e9453d4d2cfb94b224bc3b
Mitch Boyer <mitch@mitchboyer.com>
2014-02-16 18:47:17 -0500

    Add user association to pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -9,32 +11,32 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-      if @pin.save
-				redirect_to @pin, notice: 'Pin was successfully created.'
-      else
-				render action: 'new'
-      end
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-      if @pin.update(pin_params)
-				redirect_to @pin, notice: 'Pin was successfully updated.'
-      else
-				render action: 'edit'
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-		redirect_to pins_url
+    redirect_to pins_url
   end
 
   private
@@ -43,8 +45,15 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      if @pin.nil?
+				redirect_to pins_path, notice: "Not authorized to edit this pin" 
+			end	
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
