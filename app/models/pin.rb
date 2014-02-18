72bea91d698aaabc324550ff61892bc87603f358
Mitch Boyer <mitch@mitchboyer.com>
2014-02-18 17:24:15 -0500

    Added name field to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      # attr_accessible :image
      has_attached_file :image, :styles => { :large => "700x700>",:medium => "300x300>", :thumb => "100x100>" }
+
+     validates :image, presence: true
+  	validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217022845) do
+ActiveRecord::Schema.define(version: 20140218220816) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140217022845) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
